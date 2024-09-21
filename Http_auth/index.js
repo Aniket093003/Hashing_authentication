7653b7e227f66416b3cfcae738339924b3589075
Aniket093003 <aniketkainth98105@gmail.com>
2024-09-21 09:28:03 +0530

    Http_Auth

--- Http_auth/index.js
+++ Http_auth/index.js
@@ -1,65 +1,58 @@
-import express from 'express'
-import dotenv from "dotenv"
-dotenv.config({path: './.env'})
-import userModel from './user.js'
-import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import userModel from "./user.js";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-// console.log(process.env.JWT_SECRET);
-
-const JWT_SECRET = "aajbfaj"
-
-mongoose.connect(`mongodb+srv://aniketkainth98107:zKcbtlNqEzF9OEYM@cluster0.hwudb.mongodb.net/Users`)
+mongoose.connect(
+  `mongodb+srv://aniketkainth98107:zKcbtlNqEzF9OEYM@cluster0.hwudb.mongodb.net/Users`
+);
 const app = express();
 app.use(express.json());
 
-app.post("/signup", async (req, res) =>  {
-    const {name, email, password} = req.body;
+app.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
 
-     if(!email || !password || !name){
-        return res.status(411).json({
-            Error: true,
-            message: "All feilds {email, password, name} required"  
-        })
+  if (!email || !password || !name) {
+    return res.status(411).json({
+      Error: true,
+      message: "All feilds {email, password, name} required",
+    });
+  }
+  try {
+    const existingUser = await userModel.findOne({
+      email,
+    });
+    if (existingUser) {
+      return res.json({
+        message: "you are already signed up",
+      });
     }
-    try { 
 
-        const existingUser = await userModel.findOne({
-            email,
-        })
-        if (existingUser){
-            return res.json({
-                message: "you are already signed up"
-            })
-        }
-        
+    const hashedPassword = await bcrypt.hash(password, 5);
 
-        const hashedPassword = await bcrypt.hash(password, 5);
+    const user = await userModel.create({
+      email,
+      password: hashedPassword,
+      name,
+    });
 
-        const user = await userModel.create({
-            email,
-            password: hashedPassword,
-            name,
-        });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    console.log(token);
 
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
-        console.log(token);
-        
-        res.json({
-            message: "Congratulation, You are signed up",user,token
-        })
-        
-    } catch (error) {
-        console.error(error); // Log the error for debugging
-        res.status(500).json({ Error: true, message: "Internal server error" });
-    }
-})
+    res.json({
+      message: "Congratulation, You are signed up",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    res.status(500).json({ Error: true, message: "Internal server error" });
+  }
+});
 
-
-app.post("/signin", (req, res) =>{
-
-})
-
+app.post("/signin", (req, res) => {});
 
 app.listen(process.env.PORT);
---
+++ Try_catch/index.js
@@ -0,0 +1,9 @@
+function length (val){
+    return val.length;
+};
+
+ try {
+    console.log(length("aniket"));
+} catch (error) {
+    console.log("error came", error)
+}
